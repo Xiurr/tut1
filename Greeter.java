1a10ceea508e9aca081c28e48a55cf9a9a2ecb8f
Xiurr <526931608@qq.com>
2021-09-13 22:25:05 +0800

    Fix bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,7 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    args = new String[]{"en","NZ","LiXiuhan"};
     String language = args[0];
     String country = args[1];
     String name = args[2];
